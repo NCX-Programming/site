8e6ba8f333f338fbeff96c2042b2addab4ae0683
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-04-23 20:35:46 -0400

    Auto update the copyright year

--- JS/theme.js
+++ JS/theme.js
@@ -1,46 +1,55 @@
-window.onload = function() {
-  showTheme()
-
-  var lightButton = document.getElementById("lightTheme");
-  var roseButton = document.getElementById("roseTheme");
-  var darkButton = document.getElementById("darkTheme");
-  var blackButton = document.getElementById("blackTheme");
-
-  lightButton.onclick = function() {
-      chooseTheme("");
-      return false;
-  }
-
-  roseButton.onclick = function() {
-      chooseTheme("/CSS/rose.css");
-      return false;
-  }
-
-  darkButton.onclick = function() {
-      chooseTheme("/CSS/dark.css");
-      return false;
-  }
-
-  blackButton.onclick = function() {
-      chooseTheme("/CSS/black.css");
-      return false;
-  }
+if (window.addEventListener) // W3C standard
+{
+  window.addEventListener('load', themeButtons, false);
+} 
+else if (window.attachEvent) // Microsoft
+{
+  window.attachEvent('onload', themeButtons);
 }
 
+function themeButtons() {
+    showTheme();
+
+    var lightButton = document.getElementById("lightTheme");
+    var roseButton = document.getElementById("roseTheme");
+    var darkButton = document.getElementById("darkTheme");
+    var blackButton = document.getElementById("blackTheme");
+
+    lightButton.onclick = function() {
+        chooseTheme("");
+        return false;
+    }
+
+    roseButton.onclick = function() {
+        chooseTheme("/CSS/rose.css");
+        return false;
+    }
+
+    darkButton.onclick = function() {
+        chooseTheme("/CSS/dark.css");
+        return false;
+    }
+
+    blackButton.onclick = function() {
+        chooseTheme("/CSS/black.css");
+        return false;
+    }
+}
 function swapStyleSheet(sheet) {
-  document.getElementById("pagestyle").setAttribute("href", sheet);
+    document.getElementById("pagestyle").setAttribute("href", sheet);
 }
 function getTheme(){
-  var value = localStorage.getItem('theme');
-  return value;
+    var value = localStorage.getItem('theme');
+    return value;
 }
 function setTheme(theme){
-  localStorage.setItem('theme', theme);
+    localStorage.setItem('theme', theme);
 }
 function chooseTheme(theme){
-  swapStyleSheet(theme);
-  setTheme(theme);
+    swapStyleSheet(theme);
+    setTheme(theme);
 }
 function showTheme(){
-  swapStyleSheet(getTheme());
+    swapStyleSheet(getTheme());
 }
+  
