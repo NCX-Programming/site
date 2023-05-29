5b61b30b6bfc440fb6e04a839c4befefa27b0c12
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2023-05-28 23:03:14 -0400

    Updated to Bootstrap v5.3 Alpha and used color modes feature to add a dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
 window.addEventListener('load', setFooterDate, false);
+window.addEventListener('load', loadTheme, false);
 
+// Auto update the footer date so I don't have to do it myself
 function setFooterDate() {
     let footerDate = document.getElementById("footerDate");
     footerDate.textContent = new Date().getFullYear().toString();
+}
+
+// Load saved theme automatically on load
+function loadTheme() {
+    var theme = localStorage.getItem('theme');
+    document.documentElement.setAttribute('data-bs-theme', theme)
+}
+
+// Set the theme using the navbar item
+function chooseTheme(theme){
+    document.documentElement.setAttribute('data-bs-theme', theme)
+    localStorage.setItem('theme', theme);
 }
